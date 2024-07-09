cbce7d518d26fd0e33223138075be44aba0aa8fb
siwakorn.k <siwakorn.k@blendata.co>
2024-07-09 19:00:40 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeadersComponent } from './headers/headers.component';
-import { CustomerModule } from './customer/customer.module';
-import { SellerModule } from './seller/seller.module';
-import { SignupModule } from './signup/signup.module';
 import { HttpClientModule } from '@angular/common/http';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadersComponent,
-    NotFoundComponent
+    CalculatorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CustomerModule,
-    SellerModule,
-    SignupModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
